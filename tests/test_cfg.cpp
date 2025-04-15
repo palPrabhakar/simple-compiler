7ceb8eb05482dbb924e8f67bd3a2dddfe8c6acb5
Prabhakar Pal <prabhakar.pal00@gmail.com>
2025-04-15 23:23:46 +0530

    added more test cases in test_cfg

--- tests/test_cfg.cpp
+++ tests/test_cfg.cpp
@@ -96,3 +96,133 @@ TEST(CFGTest, CFG1dconv) {
     EXPECT_EQ(func->GetBlockSize(), 1);
     CHECK_SUCC(0, 0)
 }
+
+TEST(CFGTest, CFGRiemann) {
+    READ_PROGRAM("../tests/bril/riemann.json")
+    EXPECT_EQ(program->GetSize(), 5);
+    BUILD_CFG
+    sc::Block *block = nullptr;
+    sc::Function *func = nullptr;
+
+    // main
+    func = program->GetFunction(0);
+    EXPECT_EQ(func->GetBlockSize(), 1);
+    CHECK_SUCC(0, 0)
+
+    // square_function
+    func = program->GetFunction(1);
+    EXPECT_EQ(func->GetBlockSize(), 1);
+    CHECK_SUCC(0, 0)
+
+    // left_riemann
+    func = program->GetFunction(2);
+    EXPECT_EQ(func->GetBlockSize(), 4);
+    CHECK_SUCC(0, 1)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 1)
+    CHECK_SUCC(3, 0)
+
+    // left_riemann
+    func = program->GetFunction(3);
+    EXPECT_EQ(func->GetBlockSize(), 4);
+    CHECK_SUCC(0, 1)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 1)
+    CHECK_SUCC(3, 0)
+
+    // midpoint_riemann
+    func = program->GetFunction(4);
+    EXPECT_EQ(func->GetBlockSize(), 4);
+    CHECK_SUCC(0, 1)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 1)
+    CHECK_SUCC(3, 0)
+}
+
+TEST(CFGTest, CFGGol) {
+    READ_PROGRAM("../tests/bril/gol.json")
+    EXPECT_EQ(program->GetSize(), 9);
+    BUILD_CFG
+    sc::Block *block = nullptr;
+    sc::Function *func = nullptr;
+
+    // main
+    func = program->GetFunction(0);
+    EXPECT_EQ(func->GetBlockSize(), 1);
+    CHECK_SUCC(0, 0)
+
+    // next_board
+    func = program->GetFunction(1);
+    EXPECT_EQ(func->GetBlockSize(), 4);
+    CHECK_SUCC(0, 1)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 1)
+    CHECK_SUCC(3, 0)
+
+    // next_cell
+    func = program->GetFunction(2);
+    EXPECT_EQ(func->GetBlockSize(), 7);
+    CHECK_SUCC(0, 1)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 2)
+    CHECK_SUCC(3, 2)
+    CHECK_SUCC(4, 1)
+    CHECK_SUCC(5, 1)
+    CHECK_SUCC(6, 0)
+
+    // test_neighbors
+    func = program->GetFunction(3);
+    EXPECT_EQ(func->GetBlockSize(), 4);
+    CHECK_SUCC(0, 1)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 1)
+    CHECK_SUCC(3, 0)
+
+    // alive
+    func = program->GetFunction(4);
+    EXPECT_EQ(func->GetBlockSize(), 15);
+    CHECK_SUCC(0, 2)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 2)
+    CHECK_SUCC(3, 1)
+    CHECK_SUCC(4, 1)
+    CHECK_SUCC(5, 2)
+    CHECK_SUCC(6, 2)
+    CHECK_SUCC(7, 1)
+    CHECK_SUCC(8, 1)
+    CHECK_SUCC(9, 2)
+    CHECK_SUCC(10, 2)
+    CHECK_SUCC(10, 2)
+    CHECK_SUCC(11, 1)
+    CHECK_SUCC(12, 2)
+    CHECK_SUCC(13, 1)
+    CHECK_SUCC(14, 0)
+
+    // rand
+    func = program->GetFunction(5);
+    EXPECT_EQ(func->GetBlockSize(), 1);
+    CHECK_SUCC(0, 0)
+
+    // rand_array
+    func = program->GetFunction(6);
+    EXPECT_EQ(func->GetBlockSize(), 4);
+    CHECK_SUCC(0, 1)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 1)
+    CHECK_SUCC(3, 0)
+
+    // print_array
+    func = program->GetFunction(7);
+    EXPECT_EQ(func->GetBlockSize(), 4);
+    CHECK_SUCC(0, 1)
+    CHECK_SUCC(1, 2)
+    CHECK_SUCC(2, 1)
+    CHECK_SUCC(3, 0)
+
+    // mod
+    func = program->GetFunction(8);
+    EXPECT_EQ(func->GetBlockSize(), 3);
+    CHECK_SUCC(0, 2)
+    CHECK_SUCC(1, 1)
+    CHECK_SUCC(2, 0)
+}
