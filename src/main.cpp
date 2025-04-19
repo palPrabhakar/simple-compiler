63af174105cb8f09afbddbae894d79ede5bd0242
Prabhakar Pal <prabhakar.pal00@gmail.com>
2025-04-19 13:43:26 +0530

    added quick test for transformers

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,17 @@ std::unique_ptr<Program> ParseProgram(sjp::Json);
 }
 
 int main(int argc, char *argv[]) {
+    std::unique_ptr<sc::Program> program;
     if (argc < 2) {
-        std::cout << "Input file not provided. Usage ./sc <bril_file.json>\n";
-        return 0;
+        auto json_parser = sjp::Parser(std::cin);
+        auto data = json_parser.Parse();
+        program = sc::ParseProgram(data);
+    } else {
+        std::ifstream ifs(argv[1]);
+        auto json_parser = sjp::Parser(ifs);
+        auto data = json_parser.Parse();
+        program = sc::ParseProgram(data);
     }
-    std::ifstream ifs(argv[1]);
-    auto json_parser = sjp::Parser(ifs);
-    auto data = json_parser.Parse();
-    auto program = sc::ParseProgram(data);
     auto ir_transformer = sc::EarlyIRTransformer();
     program = ir_transformer.Transform(std::move(program));
     program = sc::BuildCFG(std::move(program));
